c177a782395a6d3cb9dc14e7456dadf389c1501b
sakac6401 <sakasyi6401@yahoo.co.jp>
2011-07-07 22:09:07 +0900

    aaa

---
+++ Calculas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calcul
+{
+    static class Calculas
+    {
+        public static int Sign(int val)
+        {
+            if (val > 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
