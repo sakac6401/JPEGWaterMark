d39493e19badfd86ec0983588d05b136a697c2d4
sakac6401 <sakasyi6401@yahoo.co.jp>
2011-08-19 17:50:27 +0900

    aaaaaaaaaaa

---
+++ Loging.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    static class Loging
+    {
+        public static void WriteLog(int[,] data)
+        {
+            
+        }
+    }
+}
